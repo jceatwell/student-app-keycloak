8f84c2c9685e7ad0fadbab5b8fa4be2bc98d9fee
John Eatwell <jceatwell@gmail.com>
2022-06-21 22:52:39 +0200

    add base start.spring.io project
    Following Spring Dependencies added:
    - Spring Web
    - Spring Data JPA
    - MySQL Driver
    - Spring Security
    - Thymeleaf

---
+++ src/main/java/com/infybuzz/studentappkeycloak/StudentAppKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.infybuzz.studentappkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infybuzz/studentappkeycloak/StudentAppKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infybuzz.studentappkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
